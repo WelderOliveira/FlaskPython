04b9a17b457c2d8a715c6915ad674cb8234fb06e
welder2 <weldersilva326@gmail.com>
2021-03-14 16:15:34 -0300

    Rest Api com autenticação

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-from main import Pessoas, Atividades
+from main import Pessoas, Atividades, Usuarios
+from flask_httpauth import HTTPBasicAuth
 
+auth = HTTPBasicAuth()
 app = Flask(__name__)
 api = Api(app)
 
+# USUARIOS = {
+#     'Welder': '123',
+#     'galleani': '321'
+# }
+
+# auth.verify_password
+# def verificacao(login, senha):
+#     if not (login, senha):
+#         return False
+#     return USUARIOS.get(login) == senha
+
+@auth.verify_password
+def verificacao(login, senha):
+    if not (login, senha):
+        return False
+    return Usuarios.query.filter_by(login=login, senha=senha).first()
+
 class Pessoa(Resource):
+    @auth.login_required
     def get(self, nome):
         pessoa = Pessoas.query.filter_by(nome=nome).first()
         try:
@@ -44,6 +64,7 @@ class Pessoa(Resource):
         }
 
 class ListaPessoas(Resource):
+    @auth.login_required
     def get(self):
         pessoas = Pessoas.query.all()
         response = [{'id': i.id, 'nome': i.nome,'idade': i.idade} for i in pessoas]
--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from main import Pessoas
+from main import Pessoas, Usuarios
 
 def insere_pessoa():
     pessoa = Pessoas(nome="Welder", idade=21)
@@ -20,8 +20,20 @@ def exclui_pessoa():
     pessoa = Pessoas.query.filter_by(nome='Welder').first()
     pessoa.delete()
 
+def insere_usuario(login, senha):
+    usuario = Usuarios(login=login, senha=senha)
+    usuario.save()
+
+def consulta_usuario():
+    usuario = Usuarios.query.all()
+    print(usuario)
+
 if __name__ == '__main__':
-    insere_pessoa()
+    # insere_usuario('Welder', '12345')
+    # insere_usuario('Valdirene', '54321')
+
+    consulta_usuario()
+    # insere_pessoa()
     # consulta_pessoa()
     # altera_pessoa()
     # exclui_pessoa()
